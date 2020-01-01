9893a44700e7ffe66f94ac9528a33a6208e05bcd
Jose Molina Colmenero <jose@smsdata.es>
2020-01-01 13:53:26 +0100

    Improve administration panel

--- src/screens/AdminScreen.js
+++ src/screens/AdminScreen.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { Grid } from "semantic-ui-react";
+import {
+    Button,
+    Form,
+    Grid,
+    Input
+} from "semantic-ui-react";
 import ConfigurationField from "../components/ConfigurationField";
 import { getHoustecaContract } from "../utils/contracts";
 import { getDefaultAccount } from "../utils/web3";
@@ -14,35 +19,41 @@ class AdminScreen extends React.Component {
         houstecaFeeRatio: 0,
         newLocalNode: {
             address: '',
-            minimumFeeAmount: 0,
             feeRatio: 0
         },
-
+        newToken: {
+            symbol: '',
+            address: ''
+        }
     };
 
     componentDidMount = async () => {
         const contract = await getHoustecaContract();
         const defaultAccount = await getDefaultAccount();
         const isAdmin = await contract.methods.isAdmin(defaultAccount).call();
-        const houstecaFeeRatio = await contract.methods._houstecaFeeRatio().call();
+        const houstecaFeeRatio = await contract.methods._houstecaFeeRatio().call() / 1e18;
         this.setState({contract, defaultAccount, hasPermission: isAdmin, houstecaFeeRatio});
     };
 
     addAdmin = async () => {
         const address = this.state.newAdmin;
-        this.state.contract.methods.addAdmin(address, 254, 0, 0).send({from: this.state.defaultAccount});
+        this.state.contract.methods.addAdmin(address, 254, 0).send({from: this.state.defaultAccount});
     };
 
     addLocalNode = async () => {
-        const {address, minimumFeeAmount, feeRatio} = this.state.newLocalNode;
-        this.state.contract.methods.addAdmin(
-            address, 253, minimumFeeAmount, feeRatio
-        ).send();
+        const {address, feeRatio} = this.state.newLocalNode;
+        const ratio = (feeRatio * 1e18).toString();
+        this.state.contract.methods.addAdmin(address, 253, ratio).send({from: this.state.defaultAccount});
+    };
+
+    addToken = async () => {
+        const {address, symbol} = this.state.newToken;
+        this.state.contract.methods.addToken(symbol, address).send({from: this.state.defaultAccount});
     };
 
     changeHoustecaFeeRatio = async () => {
-        const ratio = this.state.houstecaFeeRatio;
-        this.state.contract.methods.setHoustecaFeeRatio(ratio * 1e18).send({from: this.state.defaultAccount});
+        const ratio = (this.state.houstecaFeeRatio * 1e18).toString();
+        this.state.contract.methods.setHoustecaFeeRatio(ratio).send({from: this.state.defaultAccount});
     };
 
     render() {
@@ -68,6 +79,71 @@ class AdminScreen extends React.Component {
                                         defaultValue={houstecaFeeRatio}/>
                     <br/>
                     <br/>
+                    <Form>
+                        <h3>Nuevo token</h3>
+                        <Form.Group widths='equal' style={{minWidth: 750}}>
+                            <Form.Field
+                                control={Input}
+                                label='Símbolo'
+                                size="big"
+                                placeholder='Símbolo del token'
+                                onChange={event => this.setState({
+                                    newToken: {
+                                        ...this.state.newToken,
+                                        symbol: event.target.value
+                                    }
+                                })}
+                            />
+                            <Form.Field
+                                control={Input}
+                                label='Contrato'
+                                size="big"
+                                placeholder='Dirección del contrato del token ERC20 o ERC777'
+                                onChange={event => this.setState({
+                                    newToken: {
+                                        ...this.state.newToken,
+                                        address: event.target.value
+                                    }
+                                })}
+                            />
+                        </Form.Group>
+                        <br/>
+                        <Form.Field control={Button} color="blue" onClick={this.addToken}>Enviar</Form.Field>
+                    </Form>
+                    <br/>
+                    <br/>
+                    <Form>
+                        <h3>Nuevo nodo local</h3>
+                        <Form.Group widths='equal' style={{minWidth: 750}}>
+                            <Form.Field
+                                control={Input}
+                                label='Dirección'
+                                size="big"
+                                placeholder='Dirección Ethereum'
+                                onChange={event => this.setState({
+                                    newLocalNode: {
+                                        ...this.state.newLocalNode,
+                                        address: event.target.value
+                                    }
+                                })}
+                            />
+                            <Form.Field
+                                control={Input}
+                                label='Fee %'
+                                size="big"
+                                type="number"
+                                placeholder='Fee % for the new local node'
+                                onChange={event => this.setState({
+                                    newLocalNode: {
+                                        ...this.state.newLocalNode,
+                                        feeRatio: event.target.value
+                                    }
+                                })}
+                            />
+                        </Form.Group>
+                        <br/>
+                        <Form.Field control={Button} color="blue" onClick={this.addLocalNode}>Enviar</Form.Field>
+                    </Form>
                 </Grid.Column>
             </Grid>
         );
